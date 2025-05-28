54dd1b448614e2c311c6d5cf7ffdcc84f777d6c9
digiabhi <singhabhishek111213@gmail.com>
2025-05-28 18:05:58 +0530

    Added hotel service

---
+++ HotelService/src/config/config.js
@@ -0,0 +1,11 @@
+const config = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_NAME || 'airbnb_dev',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+  },
+};
+
+export default config;
