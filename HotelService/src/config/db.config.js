73ea67eb323d85a428e6c39f67ee68bbc9e012b8
digiabhi <singhabhishek111213@gmail.com>
2025-05-29 15:09:42 +0530

    Added ratings to hotels table using migrations

--- HotelService/src/config/db.config.js
+++ HotelService/src/config/db.config.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 const config = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'airbnb_dev',
-    host: process.env.DB_HOST || 'localhost',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
 };
